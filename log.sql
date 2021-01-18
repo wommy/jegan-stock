1f1f599905ae0b3bf8b7c1bf47662e308f3b2d3a
wommy <tom@technomad.media>
2021-01-18 16:04:14 -0500

    pushit fixup

--- log.sql
+++ log.sql
@@ -2,17 +2,17 @@
 
 CREATE TABLE stock {
 	_ID int AUTO_INCREMENT PRIMARY KEY,
-	name varchar NOT NULL UNIQUE,
+	name varchar(255) NOT NULL UNIQUE
 }
 
-CREATE TABLE quantity {
-	_ID int AUTO_INCREMENT PRIMARY KEY,
-	stock_ID int FOREIGN KEY REFERENCES stock(_ID),
-	quantity int NOT NULL
-}
+-- CREATE TABLE quantity {
+-- 	_ID int AUTO_INCREMENT PRIMARY KEY,
+-- 	stock_ID int FOREIGN KEY REFERENCES stock(_ID),
+-- 	quantity int NOT NULL
+-- }
 
-CREATE TABLE productComponents {
-	product_ID int FOREIGN KEY REFERENCES stock(_ID),
-	material_ID int FOREIGN KEY REFERENCES stock(_ID),
-	CONSTRAINT product PRIMARY KEY (product_ID, material_ID)
-}
+-- CREATE TABLE productComponents {
+-- 	product_ID int FOREIGN KEY REFERENCES stock(_ID),
+-- 	material_ID int FOREIGN KEY REFERENCES stock(_ID),
+-- 	CONSTRAINT product PRIMARY KEY (product_ID, material_ID)
+-- }
