9810c4dbb424e9cea15023ed71a70f7b69685547
wommy <tom@technomad.media>
2021-01-18 16:52:07 -0500

    pushit fixup

---
+++ logic.js
@@ -0,0 +1,21 @@
+//  so whats the application logic?
+
+/*
+	new product
+		new row in product table
+			key: val || name: [materialARR]
+		new rows in stock table
+			for each key, vals
+				val: each, key: [0]
+*/
+
+/*
+	add quantity ( name, quantity ) =>
+		stock.name.push( quantity )
+*/
+
+/*
+	new order ( { key: val } ) => 
+		addQuantity( -1, thank_you, mailer )
+		addQuantity( -val, key )
+*/
