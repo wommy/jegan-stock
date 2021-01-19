1a835088ac6f154c1ce9deec7d7fed75e0e0c91d
wommy <tom@technomad.media>
2021-01-18 22:33:57 -0500

    got the app to play nice with atlas

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB}?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  console.log('db connected..!')
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function findAllProducts() {
+  try {
+    await client.connect()
+    await client.db("stock").collection("products")
+      .find()    
+      .forEach(console.dir)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await client.close()
+  }
+}
+
+findAllProducts()
