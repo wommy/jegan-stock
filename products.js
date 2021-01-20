99a1bf029dfd82675c2154e61ca8df5fb277eec6
wommy <tom@technomad.media>
2021-01-20 15:59:28 -0500

    tweaked some notation a lil, about to refactor

--- products.js
+++ products.js
@@ -3,3 +3,10 @@
 	{ key: "thank_you", val: [ "card", "envelope" ] },
 	{ key: "mailer", val: [ "bubble", "business_card", "sticker_1", "sticker_2", "miniprint" ]}
 ]
+/*
+	[
+		{ lighter: [ "blank", "art", "laminate", "bag" ] },
+		{ thank_you: [ "card", "envelope" ] },
+		{ mailer: [ "bubble", "business_card", "sticker_1", "sticker_2", "miniprint" ] }
+	]
+*/
--- stock.js
+++ stock.js
@@ -10,6 +10,25 @@
 	// { name: [quantity] }
 	// { : [0] },
 
+/*
+	[
+		{ lighter: [0] },
+		{ thank_you: [0] },
+		{ mailer: [0] },
+		{ blank: [0] },
+		{ art: [0] },
+		{ laminate: [0] },
+		{ bag: [0] },
+		{ card: [0] },
+		{ envelope: [0] },
+		{ bubble: [0] },
+		{ business_card: [0] },
+		{ sticker_1: [0] },
+		{ sticker_2: [0] },
+		{ miniprint: [0] }
+	]
+*/
+
 [
 	{ key: "lighter", quantityArr: [0] },
 	{ key: "thank_you", quantityArr: [0] },
