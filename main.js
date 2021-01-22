caeaf59ee03eb7513a0e0d6238fac29094e9dfd4
wommy <tom@technomad.media>
2021-01-22 11:50:14 -0500

    pushit fixup

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-document.getElementById('get').addEventListener('click', getStock)
+const axios = require('axios')
+
+async function getStock(){
+	const results = await axios.get('')
+	renderBody()
+}
+
+function renderBody(){
+	document.getElementById('app').innerHTML = `<h1> wommy working </h1>`
+}
+
+document.getElementById('getStock').addEventListener('click', getStock)
---
+++ vanrych.js
@@ -0,0 +1,70 @@
+{
+	lighter: {
+		materials: {
+			blank: {
+				quantity: [0]
+			},
+			blank: {
+				quantity: [0]
+			},
+			blank: {
+				quantity: [0]
+			},
+			blank: {
+				quantity: [0]
+			},
+		},
+		quantity: [0]
+	},
+	thank_you: {
+		materials: {
+			card: {
+				quantity: [0]
+			},
+			envelope: { quantity: [0] },
+		quantity: [0]
+		}
+	},
+	parcel: {
+		materials: {
+			mailer: {
+				quantity: [0]
+			},
+			business_card: {
+				quantity: [0]
+			},
+			miniprint: {
+				quantity: [0]
+			},
+			sticker_1: {
+				quantity: [0]
+			},
+			sticker_2: {
+				quantity: [0]
+			}
+		},
+		quantity: [0]
+	}
+}
+
+{"_id":{"$oid":"600a36d68192942035d04d7b"},"lighter":{"quantity":[0],"materials":{"blank":{"quantity":[0]},"art":{"quantity":[0]},"laminate":{"quantity":[0]},"bag":{"quantity":[0]}}},"thank_you":{"quantity":[0],"materials":{"card":{"quantity":[0]},"envelope":{"quantity":[0]}}},"parcel":{"quantity":[0],"materials":{"mailer":{"quantity":[0]},"business_card":{"quantity":[0]},"miniprint":{"quantity":[0]},"sticker_1":{"quantity":[0]},"sticker_2":{"quantity":[0]}}}}
+
+{
+	"lighter": { "quantity": [0], "materials": { 
+		"blank": { "quantity": [0] },
+		"art": { "quantity": [0] },
+		"laminate": { "quantity": [0] },
+		"bag": { "quantity": [0] }
+	} },
+	"thank_you": { "quantity": [0], "materials": { 
+		"card": { "quantity": [0] },
+		"envelope": { "quantity": [0] }
+	} },
+	"parcel": { "quantity": [0], "materials": { 
+		"mailer": { "quantity": [0] },
+		"business_card": { "quantity": [0] },
+		"miniprint": { "quantity": [0] },
+		"sticker_1": { "quantity": [0] },
+		"sticker_2": { "quantity": [0] }
+	} }
+}
