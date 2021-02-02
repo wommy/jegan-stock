6ea269e05524bbb7fe4b8743f43e6c0c7e2586fc
wommy <tom@technomad.media>
2021-02-02 18:05:25 -0500

    updated template, still needs work

--- main2.js
+++ main2.js
@@ -2,12 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('getProducts').addEventListener('click', getProducts)
 })
 
-function getProducts(e) { console.log( 'wommy working' )
-	e.preventDefault()	
-	fetch( '/api/products' ).then( res => res.json() ).
-		then( ( content ) => {
-			template = 
-			content = content.map( arr => arr.name ).map( x => `<h2>${x}</h2>`).join("")
-			document.getElementById('app').innerHTML = content
+/*
+	{
+		"_id":"601998d84903491947de0857",
+		"name":"lighter",
+		"quantity":[0],
+		"materials":[
+			{"name":"blank","quantity":[0]},
+			{"name":"art","quantity":[0]},
+			{"name":"laminate","quantity":[0]},
+			{"name":"bag","quantity":[0]}
+		]
+	},
+*/
+
+let template = `<div id="template">
+<span>${arr.name}:</span>
+<span>${arr.quantity}</span>
+</div>`
+
+function getProducts(e) { e.preventDefault()	
+	fetch( '/api/products' )
+		.then( res => res.json() )
+		.then( content => {
+			products = content.map( ea => `<div>
+			<p>${ea.name}: ${ea.quantity}, materials: ${ea.materials.map( ma => `<div>${ma.name}: ${ma.quantity}</div>` ).join("")}</p>
+		</div>` ).join("")
+			document.getElementById('app').innerHTML = products
 		}).catch( err => console.error )
 }
