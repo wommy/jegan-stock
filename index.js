341be3d40641528cb8dfee87e52f165e396ddcfc
wommy <tom@technomad.media>
2021-02-01 19:17:14 -0500

    post working

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ async function get( collectionName ) {
 		.find().forEach(console.dir)
 }
 
+async function postProduct() {
+	await db.collection("products")
+		.insertOne({
+			thank_you: { 
+				quantity: [0], 
+				materials: { 
+					card: { quantity: [0] },
+					envelope: { quantity: [0] }
+				}
+			}
+		})
+}
+
 async function connDB() {
 	try {
 		await client.connect()
 		db = client.db("jegan")
+		await get("products")
+		await postProduct()
 		await get("products")
 	} catch (err) { throw err
 	} finally { await client.close() }
